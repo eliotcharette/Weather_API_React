158a055f8c1197ce62c66e98c8631e775a162bc8
Eliot Charette <eliotcharette@gmail.com>
2018-09-26 16:39:37 -0700

    Forgot brackets in import for javascript object

---
+++ src/components/chart.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+  return _.round(_.sum(data)/data.length)
+}
+
+export default (props) => {
+  return (
+    <div>
+      <Sparklines height={120} width={180} data={props.data}>
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
+    </div>
+  );
+}
+
+
+
+// <Sparklines height={120} width={180} data={humidity}>
+//   <SparklinesLine color="red" />
+// </Sparklines>
+// <Sparklines height={120} width={180} data={pressure}>
+//   <SparklinesLine color="green" />
+// </Sparklines>
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+
+
+
     return (
-      <tr>
-        <td>{cityData.city.list}</td>
+      <tr key={name}>
+        <td>{name}</td>
+        <td><Chart data={temps} color="orange" units='ºK'/></td>
+        <td><Chart data={pressures} color="green" units='hPa'/></td>
+        <td><Chart data={humidities} color="grey" units='%' /></td>
       </tr>
-    )
+    );
   }
   render() {
     return (
@@ -15,9 +26,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
